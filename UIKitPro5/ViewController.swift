6923c16a6dba837f82e50ac652735dc9aace8c73
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-07-18 13:09:29 -0700

    Auto Layout anchors

--- UIKitPro5/ViewController.swift
+++ UIKitPro5/ViewController.swift
@@ -48,15 +48,30 @@ class ViewController: UIViewController {
         view.addSubview(label4)
         view.addSubview(label5)
         
-        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+//        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+//        
+//        for label in viewsDictionary.keys {
+//            view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+//        }
+//        
+//        // اعداد برای اعمال محدودیت
+//        let metrics = ["HeightLabel": 80]
+//        view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(HeightLabel@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
         
-        for label in viewsDictionary.keys {
-            view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+        var previous: UILabel?
+        
+        for label in [label1, label2, label3, label4, label5] {
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.heightAnchor.constraint(equalToConstant: 80).isActive = true
+            
+            // نسبت به قبلی
+            if let previous = previous {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+            previous = label
         }
-        
-        // اعداد برای اعمال محدودیت
-        let metrics = ["HeightLabel": 80]
-        view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(HeightLabel@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
 
     }
 }
